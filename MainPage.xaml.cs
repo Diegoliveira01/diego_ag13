0991f8596f6b6d956a71d4fe07dcf4fc466ddbcd
Diegoliveira01 <diego.arodrigues08@gmail.com>
2025-06-01 17:49:45 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace MauiAppHotel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSobreClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SobrePage());
+        }
+
+        private void OnAdicionarAdulto(object sender, EventArgs e)
+        {
+            int valorAtual = int.TryParse(adultosEntry.Text, out int result) ? result : 0;
+            valorAtual++;
+            adultosEntry.Text = valorAtual.ToString();
+        }
+
+        private void OnRemoverAdulto(object sender, EventArgs e)
+        {
+            int valorAtual = int.TryParse(adultosEntry.Text, out int result) ? result : 0;
+            valorAtual = Math.Max(0, valorAtual - 1);
+            adultosEntry.Text = valorAtual.ToString();
+        }
+
+        private void OnAdicionarCrianca(object sender, EventArgs e)
+        {
+            int valorAtual = int.TryParse(criancasEntry.Text, out int result) ? result : 0;
+            valorAtual++;
+            criancasEntry.Text = valorAtual.ToString();
+        }
+
+        private void OnRemoverCrianca(object sender, EventArgs e)
+        {
+            int valorAtual = int.TryParse(criancasEntry.Text, out int result) ? result : 0;
+            valorAtual = Math.Max(0, valorAtual - 1);
+            criancasEntry.Text = valorAtual.ToString();
+        }
+    }
+}
